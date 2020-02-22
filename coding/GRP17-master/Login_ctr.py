c511f4a9f54819b751534728ffb49c1d42538f08
Yiming Li <1870274845@qq.com>
2020-02-22 11:38:29 +0800

    Added database specification document.

--- coding/GRP17-master/Login_ctr.py
+++ coding/GRP17-master/Login_ctr.py
@@ -1,3 +1,6 @@
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from Login_View import login_View
 from ModulePage_View import modulePage_view
 from basicMainWindow_View import basicMainWindow_view
 from basicMainWindow_Ctr import basicMainWindow_Ctr
@@ -7,9 +10,13 @@ from basicMainWindow_Ctr import basicMainWindow_Ctr
 class login_Ctr():
 
     def __init__(self):
+        #super(login_Ctr, self).__init__()
         # TODO: test login input, link to data
         # bindModel = login_Model()
-        self.loginView = None
+        # self.loginView = None
+        self.loginView = login_View()
+        #self.loginView.pushButton.clicked.connect(self.login)
+        #self.loginView.toolButton.clicked.connect(self.forgetPwd)
 
         # create main window, set view and controller
         self.mainWindow = basicMainWindow_view()
@@ -22,12 +29,26 @@ class login_Ctr():
 
         self.mainWindowCtr.setWindow(self.tmView)
 
+    #def login(self):
+    #    print("login")
+    #    self.loginView.login_Signal.emit()
+
+    #def forgetPwd(self):
+    #    self.loginView.forgetPwd_Signal.emit()
+    #    print("forget")
+        """
+               Slot documentation goes here.
+        """
+        # TODO: show the forget pw dialog
+
     def setView(self, loginView):
         self.loginView = loginView
         self.connectSlot()
 
     def connectSlot(self):
         self.loginView.login_Signal.connect(self.enterMainPage)
+        #self.loginView.login_Signal.connect(self.login)
+        #self.loginView.forgetPwd_Signal.connect(self.forgetPwd)
 
     def enterMainPage(self):
         self.loginView.hide()
