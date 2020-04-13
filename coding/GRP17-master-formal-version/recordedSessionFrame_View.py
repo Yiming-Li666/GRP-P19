e9b867f8b984b66a76e4ba3dd3c1b91778d81a11
FatShion_FTD <553949423@qq.com>
2020-04-13 21:51:19 +0800

    Complete recorded page

--- coding/GRP17-master-formal-version/recordedSessionFrame_View.py
+++
@@ -1,7 +0,0 @@
-from resources.teacherUIPY.recordedSession_Frame import recordedSession_Frame
-from PyQt5.QtWidgets import QFrame
-class recordedSessionFrame_View(QFrame, recordedSession_Frame):
-
-    def __init__(self):
-        super(recordedSessionFrame_View, self).__init__()
-        self.setupUi(self)
