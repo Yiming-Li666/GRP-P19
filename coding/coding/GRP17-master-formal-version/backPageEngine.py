cab6eaab947f12d38fe34c484bbfb32a161b9faf
Yiming Li <1870274845@qq.com>
2020-04-21 19:47:04 +0800

    Changed file directory.

--- coding/coding/GRP17-master-formal-version/backPageEngine.py
+++
@@ -1,9 +0,0 @@
-from sessionPage_View import sessionPage_View
-
-class backPageEngine():
-    def backToModule(self, mainwindow, mainWindowCtr):
-        self.ModuleView = sessionPage_View()
-        self.ModuleView.setMainWindow(mainwindow)
-
-        mainWindowCtr.setWindow(self.ModuleView)
-
